b166b8ca4c34cf8bd818c8416959a08b7ccd591a
htamta <36506995+hussaintt@users.noreply.github.com>
2023-08-25 18:25:52 +0300

    Add conversation methods and reverse button

--- app/src/main/java/com/hussaintt55/currencyapp/ui/fragments/CurrencyConversionFragment.kt
+++ app/src/main/java/com/hussaintt55/currencyapp/ui/fragments/CurrencyConversionFragment.kt
@@ -1,6 +1,8 @@
 package com.hussaintt55.currencyapp.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +10,12 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Spinner
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.hussaintt55.currencyapp.R
@@ -25,7 +32,17 @@ class CurrencyConversionFragment : Fragment() {
 
     var myView:View?=null
     var details:Button?=null
-
+    var firstCurrencyText:EditText?=null
+    var secondCurrencyText:TextView?=null
+    var defaultValue=1
+    var CurrentValue = 0.0
+    var Currency1SelectedValue = 0.0
+    var Currency2SelectedValue = 0.0
+    var Currency1SelectedKey = ""
+    var Currency2SelectedKey = ""
+    var Currency1Possition = 2
+    var Currency2Possition = 1
+    var reverse:ImageView?=null
     lateinit var viewModel: MainActivityViewModel
 
     override fun onCreateView(
@@ -48,36 +65,70 @@ class CurrencyConversionFragment : Fragment() {
     }
 
     var myHashMap:HashMap<String,Double>?=null
+    var spinner:Spinner?=null
+    var spinner2:Spinner?=null
     fun setupSpinner(){
-        val spinner = myView?.findViewById<Spinner>(R.id.spinner)
-        val spinner2 = myView?.findViewById<Spinner>(R.id.spinner2)
-        if (spinner != null) {
-            initSpinner(spinner,1)
-        }
-        if (spinner2 != null) {
-            initSpinner(spinner2,2)
+         spinner = myView?.findViewById<Spinner>(R.id.spinner)
+         spinner2 = myView?.findViewById<Spinner>(R.id.spinner2)
+         firstCurrencyText = myView?.findViewById(R.id.fromValue)
+         secondCurrencyText = myView?.findViewById(R.id.ToValue)
+         reverse = myView?.findViewById(R.id.reverse)
+        initSpinner()
+        initEditText()
+        setupReverse()
+
+
+    }
+
+    private fun setupReverse() {
+        reverse?.setOnClickListener {
+            reverseValues()
         }
     }
+
+    private fun initEditText() {
+        firstCurrencyText?.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) { }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) {
+                    val desiredAmount = s.toString().toDouble()
+                    secondCurrencyText?.text = calculateCurrentValue(desiredAmount,Currency1SelectedValue,Currency2SelectedValue).toString()
+                }
+            }
+        })
+    }
+
     fun navigateToDetails(){
         findNavController().navigate(R.id.action_currencyConversionFragment_to_historicalFragment)
     }
 
-    fun initSpinner(spinner: Spinner,state:Int){
+    fun initSpinner(){
         myHashMap.let {
             val adapter = ArrayAdapter(requireContext(), R.layout.my_custom_spinner_item, myHashMap!!.keys.toList())
             adapter.setDropDownViewResource(R.layout.my_custom_spinner_item)
             spinner?.adapter = adapter
+            spinner2?.adapter = adapter
+
             spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    val selectedItem = spinner?.selectedItem.toString() // This is the selected item label
-                    val selectedValue = myHashMap!![selectedItem] // This is the corresponding value
-                    if (state == 1){
-                        println("test123 $selectedValue from")
-                    }
-                    else if (state ==2){
-                        println("test123 $selectedValue to")
+                    Currency1SelectedKey = spinner?.selectedItem.toString() // This is the selected item label
+                    Currency1SelectedValue = myHashMap!![Currency1SelectedKey]!! // This is the corresponding value
+                    Currency1Possition = spinner?.selectedItemPosition!!
+                    secondCurrencyText?.text =(calculateCurrentValue(1.0,Currency1SelectedValue,Currency2SelectedValue)).toString()
+                }
 
-                    }
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // Handle case when nothing is selected, if necessary
+                }
+            }
+
+            spinner2?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                     Currency2SelectedKey = spinner2?.selectedItem.toString() // This is the selected item label
+                     Currency2SelectedValue = myHashMap!![Currency2SelectedKey]!! // This is the corresponding value
+                     Currency2Possition = spinner2?.selectedItemPosition!!
+                    secondCurrencyText?.text =(calculateCurrentValue(1.0,Currency1SelectedValue,Currency2SelectedValue)).toString()
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -85,5 +136,16 @@ class CurrencyConversionFragment : Fragment() {
                 }
             }
         }
+    }
+
+    fun calculateCurrentValue(Value: Double, currency1: Double, currency2: Double): Double {
+        return Value * (currency1 / currency2)
+    }
+
+    fun reverseValues(){
+        val temp1 = Currency1Possition
+        val temp2 = Currency2Possition
+        spinner?.setSelection(temp2)
+        spinner2?.setSelection(temp1)
     }
 }
