6b906c90c5e16473f1a7497703a308e922ce2807
htamta <36506995+hussaintt@users.noreply.github.com>
2023-08-25 16:03:47 +0300

    Setup conversation layout

--- app/src/main/java/com/hussaintt55/currencyapp/ui/MainActivity.kt
+++ app/src/main/java/com/hussaintt55/currencyapp/ui/MainActivity.kt
@@ -7,17 +7,11 @@ import androidx.navigation.findNavController
 import com.hussaintt55.currencyapp.R
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findNavController(R.id.currencyFragment).setGraph(R.navigation.main_nav)
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        viewModel.currencyListResponse.observe(this) {
-            println("DEBUG $it")
-        }
 
-        viewModel.Trigger(true)
 
 
     }
--- app/src/main/java/com/hussaintt55/currencyapp/ui/fragments/CurrencyConversionFragment.kt
+++ app/src/main/java/com/hussaintt55/currencyapp/ui/fragments/CurrencyConversionFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.hussaintt55.currencyapp.R
-
+import com.hussaintt55.currencyapp.ui.MainActivityViewModel
 
 
 class CurrencyConversionFragment : Fragment() {
@@ -22,6 +26,8 @@ class CurrencyConversionFragment : Fragment() {
     var myView:View?=null
     var details:Button?=null
 
+    lateinit var viewModel: MainActivityViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,10 +38,52 @@ class CurrencyConversionFragment : Fragment() {
         details?.setOnClickListener {
             navigateToDetails()
         }
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel.currencyListResponse.observe(viewLifecycleOwner) {
+        myHashMap=it.rates
+        setupSpinner()
+        }
+        viewModel.Trigger(true)
         return myView
     }
 
+    var myHashMap:HashMap<String,Double>?=null
+    fun setupSpinner(){
+        val spinner = myView?.findViewById<Spinner>(R.id.spinner)
+        val spinner2 = myView?.findViewById<Spinner>(R.id.spinner2)
+        if (spinner != null) {
+            initSpinner(spinner,1)
+        }
+        if (spinner2 != null) {
+            initSpinner(spinner2,2)
+        }
+    }
     fun navigateToDetails(){
         findNavController().navigate(R.id.action_currencyConversionFragment_to_historicalFragment)
+    }
+
+    fun initSpinner(spinner: Spinner,state:Int){
+        myHashMap.let {
+            val adapter = ArrayAdapter(requireContext(), R.layout.my_custom_spinner_item, myHashMap!!.keys.toList())
+            adapter.setDropDownViewResource(R.layout.my_custom_spinner_item)
+            spinner?.adapter = adapter
+            spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val selectedItem = spinner?.selectedItem.toString() // This is the selected item label
+                    val selectedValue = myHashMap!![selectedItem] // This is the corresponding value
+                    if (state == 1){
+                        println("test123 $selectedValue from")
+                    }
+                    else if (state ==2){
+                        println("test123 $selectedValue to")
+
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // Handle case when nothing is selected, if necessary
+                }
+            }
+        }
     }
 }
